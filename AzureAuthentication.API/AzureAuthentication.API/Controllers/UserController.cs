cf51e427dcb6b1d8789cb858b0ee3455148e9f1e
deepaksnegi <46718177+deepaksnegi@users.noreply.github.com>
2021-01-21 20:34:18 +0530

    "implemented azure authentication"

---
+++ AzureAuthentication.API/AzureAuthentication.API/Controllers/UserController.cs
@@ -0,0 +1,65 @@
+﻿using AzureAuthentication.API.Helper.TestData;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+
+namespace AzureAuthentication.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("users")]
+    public class UserController : ControllerBase
+    {
+        #region Private Fields
+
+        private readonly ILogger<UserController> _logger;
+
+        #endregion
+
+        #region Constructor
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Fetch all users. Currently users are bieng fetched from test data.
+        /// </summary>
+        /// <returns>users</returns>
+        /// <response code="200">users</response>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(UserTestData.users);
+        }
+
+        /// <summary>
+        /// Fetch a user which is matched to given guid
+        /// </summary>
+        /// <param name="guid">user guid</param>
+        /// <returns>user</returns>
+        /// <response code="200">user</response>
+        /// <response code="400">if the user is not found</response>  
+        [Route("{guid}")]
+        [HttpGet]
+        public IActionResult Get(string guid)
+        {
+            var user = UserTestData.users.SingleOrDefault(u => u.Guid.Equals(guid));
+
+            if(user==null)
+            {
+                 return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        #endregion
+    }
+}
---
+++ AzureAuthentication.API/AzureAuthentication.API/Helper/TestData/UserTestData.cs
@@ -0,0 +1,27 @@
+﻿using AzureAuthentication.API.DTO;
+using System;
+using System.Collections.Generic;
+
+namespace AzureAuthentication.API.Helper.TestData
+{
+    public static class UserTestData
+    {
+        public static readonly IList<UserDto> users = new List<UserDto>
+        {
+            new UserDto
+            {
+                Email ="john.doe@example.com",
+                Guid= new  Guid("7966551C-DC33-446F-9FB9-8D0899CC776D").ToString(),
+                FirstName="John",
+                LastName="Doe"
+            },
+            new UserDto
+            {
+                Email ="ervin.howell@example.com",
+                Guid= new  Guid("60ef468d-440a-4d32-98a0-194b8b89b7c7").ToString(),
+                FirstName="Ervin",
+                LastName="Howell"
+            }
+        };
+    }
+}
